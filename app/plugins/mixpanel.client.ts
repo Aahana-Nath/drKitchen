dec55f4ffd1e458c22a124b7843180432397fbf3
Aahana Nath <aahana.nath@dresma.com>
2026-07-17 10:58:07 +0530

    feat: identify anonymous users for Mixpanel Users page

--- app/plugins/mixpanel.client.ts
+++ app/plugins/mixpanel.client.ts
@@ -6,6 +6,16 @@ export default defineNuxtPlugin(() => {
     record_sessions_percent: 100,
   })
 
+  // Identify anonymous visitor so they appear on the Users page
+  const distinctId = mixpanel.get_distinct_id()
+  mixpanel.identify(distinctId)
+  mixpanel.people.set_once({
+    '$first_seen': new Date().toISOString(),
+  })
+  mixpanel.people.set({
+    '$last_seen': new Date().toISOString(),
+  })
+
   return {
     provide: {
       mixpanel,
